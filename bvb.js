deddb484c9280a59743641f1b2160f4fc433b335
Create-QQQQ <64317834+Create-QQQQ@users.noreply.github.com>
2020-04-26 22:17:33 +0800

    Add files via upload

---
+++ bvb.js
@@ -0,0 +1,22 @@
+// const md5=require("md5")
+// const password="987654"+"+&*%"
+// console.log(md5(password))
+// const jwt=require("jwt-simple");
+//生成token
+//参数一：荷载数据--个人信息
+//参数二：是一个钥匙（key）
+// const key="QIQI"
+// const  token=jwt.encode({
+//     adminName:"漆云峰",
+//     createTime:Date.now()
+// },key)
+// console.log(token)
+//
+// //解析token
+// // const  info=jwt.decode(token,key)
+// // console.log(info)
+
+// const arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+// function
+//
+// console.log(changeArr(arr,5))
